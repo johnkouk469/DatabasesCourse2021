1c7f940ff825c79c69b01d7ba46ed9e4b2baeb0a
Gwyn <harry.filis@yahoo.gr>
2021-12-12 19:24:11 +0200

    Update dump and mwb files

--- src/EVPoint_dump.sql
+++ src/EVPoint_dump.sql
@@ -5,83 +5,53 @@ SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
 
 -- -----------------------------------------------------
--- Schema EVPointDB
+-- Schema mydb
 -- -----------------------------------------------------
-DROP SCHEMA IF EXISTS `EVPointDB` ;
+-- -----------------------------------------------------
+-- Schema evpointdb
+-- -----------------------------------------------------
+DROP SCHEMA IF EXISTS `evpointdb` ;
 
 -- -----------------------------------------------------
--- Schema EVPointDB
+-- Schema evpointdb
 -- -----------------------------------------------------
-CREATE SCHEMA IF NOT EXISTS `EVPointDB` DEFAULT CHARACTER SET utf8 ;
-USE `EVPointDB` ;
+CREATE SCHEMA IF NOT EXISTS `evpointdb` DEFAULT CHARACTER SET utf8 ;
+USE `evpointdb` ;
 
 -- -----------------------------------------------------
--- Table `EVPointDB`.`User`
+-- Table `evpointdb`.`car`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `EVPointDB`.`User` (
-  `userID` CHAR(6) NOT NULL,
-  `phoneNumber` INT(10) UNSIGNED NULL,
-  `longtitude` DECIMAL(2,2) NULL,
-  `latitude` DECIMAL(2,2) NULL,
-  PRIMARY KEY (`userID`),
-  UNIQUE INDEX `phoneNumber_UNIQUE` (`phoneNumber` ASC) VISIBLE)
-ENGINE = InnoDB;
-
-
--- -----------------------------------------------------
--- Table `EVPointDB`.`Car`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `EVPointDB`.`Car` (
+CREATE TABLE IF NOT EXISTS `evpointdb`.`car` (
   `licenseNumber` CHAR(7) NOT NULL,
-  `name` VARCHAR(25) NULL,
-  `photo` BLOB NULL,
-  `notes` VARCHAR(512) NULL,
+  `name` VARCHAR(25) NULL DEFAULT NULL,
+  `photo` BLOB NULL DEFAULT NULL,
+  `notes` VARCHAR(512) NULL DEFAULT NULL,
   PRIMARY KEY (`licenseNumber`))
-ENGINE = InnoDB;
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb3;
 
 
 -- -----------------------------------------------------
--- Table `EVPointDB`.`Health`
+-- Table `evpointdb`.`chargingstation`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `EVPointDB`.`Health` (
-  `healthTimeStamp` TIMESTAMP(6) NOT NULL,
-  `engineHealth` DECIMAL(3,3) NULL,
-  `batteryHealth` DECIMAL(3,3) NULL,
-  `tirePressure` DECIMAL(3,3) NULL,
-  `tireCondition` DECIMAL(3,3) NULL,
-  `batteryPercentage` DECIMAL(3,3) NULL,
-  `Healthcol` VARCHAR(45) NULL,
-  `Car_licenseNumber` CHAR(7) NOT NULL,
-  PRIMARY KEY (`healthTimeStamp`, `Car_licenseNumber`),
-  INDEX `fk_Health_Car1_idx` (`Car_licenseNumber` ASC) VISIBLE,
-  CONSTRAINT `fk_Health_Car1`
-    FOREIGN KEY (`Car_licenseNumber`)
-    REFERENCES `EVPointDB`.`Car` (`licenseNumber`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
-
-
--- -----------------------------------------------------
--- Table `EVPointDB`.`chargingStation`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `EVPointDB`.`chargingStation` (
+CREATE TABLE IF NOT EXISTS `evpointdb`.`chargingstation` (
   `companyName` VARCHAR(25) NOT NULL,
   `latitude` DECIMAL(2,2) NOT NULL,
   `longtitude` DECIMAL(2,2) NOT NULL,
-  `meanStars` DECIMAL(1,1) NULL,
+  `meanStars` DECIMAL(1,1) NULL DEFAULT NULL,
   PRIMARY KEY (`companyName`, `latitude`, `longtitude`))
-ENGINE = InnoDB;
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb3;
 
 
 -- -----------------------------------------------------
--- Table `EVPointDB`.`Connector`
+-- Table `evpointdb`.`connector`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `EVPointDB`.`Connector` (
-  `connectorID` INT(6) NOT NULL,
-  `connectorType` ENUM('TESLATYPE2.TYPE2.CCS') NULL,
-  `availability` INT(1) NULL,
-  `Connectorcol` VARCHAR(45) NULL,
+CREATE TABLE IF NOT EXISTS `evpointdb`.`connector` (
+  `connectorID` INT NOT NULL,
+  `connectorType` ENUM('TESLA-TYPE2,TYPE2,CCS') NULL DEFAULT NULL,
+  `availability` INT NULL DEFAULT NULL,
+  `Connectorcol` VARCHAR(45) NULL DEFAULT NULL,
   `chargingStation_companyName` VARCHAR(25) NOT NULL,
   `chargingStation_latitude` DECIMAL(2,2) NOT NULL,
   `chargingStation_longtitude` DECIMAL(2,2) NOT NULL,
@@ -89,144 +59,164 @@ CREATE TABLE IF NOT EXISTS `EVPointDB`.`Connector` (
   INDEX `fk_Connector_chargingStation1_idx` (`chargingStation_companyName` ASC, `chargingStation_latitude` ASC, `chargingStation_longtitude` ASC) VISIBLE,
   CONSTRAINT `fk_Connector_chargingStation1`
     FOREIGN KEY (`chargingStation_companyName` , `chargingStation_latitude` , `chargingStation_longtitude`)
-    REFERENCES `EVPointDB`.`chargingStation` (`companyName` , `latitude` , `longtitude`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
+    REFERENCES `evpointdb`.`chargingstation` (`companyName` , `latitude` , `longtitude`))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb3;
 
 
 -- -----------------------------------------------------
--- Table `EVPointDB`.`RSA`
+-- Table `evpointdb`.`health`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `EVPointDB`.`RSA` (
-  `phoneNumber` INT(10) NOT NULL,
-  `name` VARCHAR(25) NULL,
-  `longtitude` DECIMAL(2,2) NULL,
-  `latitude` DECIMAL(2,2) NULL,
+CREATE TABLE IF NOT EXISTS `evpointdb`.`health` (
+  `healthTimeStamp` TIMESTAMP(6) NOT NULL,
+  `engineHealth` DECIMAL(3,3) NULL DEFAULT NULL,
+  `batteryHealth` DECIMAL(3,3) NULL DEFAULT NULL,
+  `tirePressure` DECIMAL(3,3) NULL DEFAULT NULL,
+  `tireCondition` DECIMAL(3,3) NULL DEFAULT NULL,
+  `batteryPercentage` DECIMAL(3,3) NULL DEFAULT NULL,
+  `Car_licenseNumber` CHAR(7) NOT NULL,
+  PRIMARY KEY (`healthTimeStamp`, `Car_licenseNumber`),
+  INDEX `fk_Health_Car1_idx` (`Car_licenseNumber` ASC) VISIBLE,
+  CONSTRAINT `fk_Health_Car1`
+    FOREIGN KEY (`Car_licenseNumber`)
+    REFERENCES `evpointdb`.`car` (`licenseNumber`))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb3;
+
+
+-- -----------------------------------------------------
+-- Table `evpointdb`.`occupiedconnector`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `evpointdb`.`occupiedconnector` (
+  `OccupiedConnectorID` INT NOT NULL,
+  `connectedCarLN` CHAR(7) NOT NULL,
+  `occupiedFrom` TIMESTAMP(6) NULL DEFAULT NULL,
+  `occupiedUntil` TIMESTAMP(6) NULL DEFAULT NULL,
+  `occupiedEstimatedUntil` TIMESTAMP(6) NULL DEFAULT NULL,
+  PRIMARY KEY (`OccupiedConnectorID`, `connectedCarLN`),
+  INDEX `carLicenseNumber_idx` (`connectedCarLN` ASC) VISIBLE,
+  CONSTRAINT `carLicenseNumber`
+    FOREIGN KEY (`connectedCarLN`)
+    REFERENCES `evpointdb`.`car` (`licenseNumber`),
+  CONSTRAINT `occupiedConnectorID`
+    FOREIGN KEY (`OccupiedConnectorID`)
+    REFERENCES `evpointdb`.`connector` (`connectorID`))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb3;
+
+
+-- -----------------------------------------------------
+-- Table `evpointdb`.`rsa`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `evpointdb`.`rsa` (
+  `phoneNumber` INT NOT NULL,
+  `name` VARCHAR(25) NULL DEFAULT NULL,
+  `longtitude` DECIMAL(2,2) NULL DEFAULT NULL,
+  `latitude` DECIMAL(2,2) NULL DEFAULT NULL,
   PRIMARY KEY (`phoneNumber`))
-ENGINE = InnoDB;
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb3;
 
 
 -- -----------------------------------------------------
--- Table `EVPointDB`.`User_owns_Car`
+-- Table `evpointdb`.`user`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `EVPointDB`.`User_owns_Car` (
+CREATE TABLE IF NOT EXISTS `evpointdb`.`user` (
+  `userID` CHAR(6) NOT NULL,
+  `phoneNumber` INT UNSIGNED NULL DEFAULT NULL,
+  `longtitude` DECIMAL(2,2) NULL DEFAULT NULL,
+  `latitude` DECIMAL(2,2) NULL DEFAULT NULL,
+  PRIMARY KEY (`userID`),
+  UNIQUE INDEX `phoneNumber_UNIQUE` (`phoneNumber` ASC) VISIBLE)
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb3;
+
+
+-- -----------------------------------------------------
+-- Table `evpointdb`.`user_calls_rsa`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `evpointdb`.`user_calls_rsa` (
+  `User_userID` CHAR(6) NOT NULL,
+  `RSA_phoneNumber` INT NOT NULL,
+  `timestamp` TIMESTAMP(6) NULL DEFAULT NULL,
+  `callerLattitude` DECIMAL(2,2) NULL DEFAULT NULL,
+  `callerLongtitude` DECIMAL(2,2) NULL DEFAULT NULL,
+  PRIMARY KEY (`User_userID`, `RSA_phoneNumber`),
+  INDEX `fk_User_has_RSA_RSA1_idx` (`RSA_phoneNumber` ASC) VISIBLE,
+  INDEX `fk_User_has_RSA_User1_idx` (`User_userID` ASC) VISIBLE,
+  CONSTRAINT `fk_User_has_RSA_RSA1`
+    FOREIGN KEY (`RSA_phoneNumber`)
+    REFERENCES `evpointdb`.`rsa` (`phoneNumber`),
+  CONSTRAINT `fk_User_has_RSA_User1`
+    FOREIGN KEY (`User_userID`)
+    REFERENCES `evpointdb`.`user` (`userID`))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb3;
+
+
+-- -----------------------------------------------------
+-- Table `evpointdb`.`user_owns_car`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `evpointdb`.`user_owns_car` (
   `User_userID` CHAR(6) NOT NULL,
   `Car_licenseNumber` CHAR(7) NOT NULL,
   PRIMARY KEY (`User_userID`, `Car_licenseNumber`),
   INDEX `fk_User_has_Car_Car1_idx` (`Car_licenseNumber` ASC) VISIBLE,
   INDEX `fk_User_has_Car_User_idx` (`User_userID` ASC) VISIBLE,
-  CONSTRAINT `fk_User_has_Car_User`
-    FOREIGN KEY (`User_userID`)
-    REFERENCES `EVPointDB`.`User` (`userID`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
   CONSTRAINT `fk_User_has_Car_Car1`
     FOREIGN KEY (`Car_licenseNumber`)
-    REFERENCES `EVPointDB`.`Car` (`licenseNumber`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
+    REFERENCES `evpointdb`.`car` (`licenseNumber`),
+  CONSTRAINT `fk_User_has_Car_User`
+    FOREIGN KEY (`User_userID`)
+    REFERENCES `evpointdb`.`user` (`userID`))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb3;
 
 
 -- -----------------------------------------------------
--- Table `EVPointDB`.`User_reviews_chargingStation`
+-- Table `evpointdb`.`user_reviews_chargingstation`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `EVPointDB`.`User_reviews_chargingStation` (
+CREATE TABLE IF NOT EXISTS `evpointdb`.`user_reviews_chargingstation` (
   `User_userID` CHAR(6) NOT NULL,
   `chargingStation_companyName` VARCHAR(25) NOT NULL,
   `chargingStation_latitude` DECIMAL(2,2) NOT NULL,
   `chargingStation_longtitude` DECIMAL(2,2) NOT NULL,
-  `stars` INT(1) NULL,
-  `comment` VARCHAR(512) NULL,
-  `date` DATE NULL,
+  `stars` INT NULL DEFAULT NULL,
+  `comment` VARCHAR(512) NULL DEFAULT NULL,
+  `date` DATE NULL DEFAULT NULL,
   PRIMARY KEY (`User_userID`, `chargingStation_companyName`, `chargingStation_latitude`, `chargingStation_longtitude`),
   INDEX `fk_User_has_chargingStation_chargingStation1_idx` (`chargingStation_companyName` ASC, `chargingStation_latitude` ASC, `chargingStation_longtitude` ASC) VISIBLE,
   INDEX `fk_User_has_chargingStation_User1_idx` (`User_userID` ASC) VISIBLE,
-  CONSTRAINT `fk_User_has_chargingStation_User1`
-    FOREIGN KEY (`User_userID`)
-    REFERENCES `EVPointDB`.`User` (`userID`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
   CONSTRAINT `fk_User_has_chargingStation_chargingStation1`
     FOREIGN KEY (`chargingStation_companyName` , `chargingStation_latitude` , `chargingStation_longtitude`)
-    REFERENCES `EVPointDB`.`chargingStation` (`companyName` , `latitude` , `longtitude`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
-
-
--- -----------------------------------------------------
--- Table `EVPointDB`.`OccupiedConnector`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `EVPointDB`.`OccupiedConnector` (
-  `OccupiedConnectorID` INT(6) NOT NULL,
-  `connectedCarLN` CHAR(7) NOT NULL,
-  `occupiedFrom` TIMESTAMP(6) NULL,
-  `occupiedUntil` TIMESTAMP(6) NULL,
-  `occupiedEstimatedUntil` TIMESTAMP(6) NULL,
-  PRIMARY KEY (`OccupiedConnectorID`, `connectedCarLN`),
-  INDEX `carLicenseNumber_idx` (`connectedCarLN` ASC) VISIBLE,
-  CONSTRAINT `carLicenseNumber`
-    FOREIGN KEY (`connectedCarLN`)
-    REFERENCES `EVPointDB`.`Car` (`licenseNumber`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `occupiedConnectorID`
-    FOREIGN KEY (`OccupiedConnectorID`)
-    REFERENCES `EVPointDB`.`Connector` (`connectorID`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
-
-
--- -----------------------------------------------------
--- Table `EVPointDB`.`User_calls_RSA`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `EVPointDB`.`User_calls_RSA` (
-  `User_userID` CHAR(6) NOT NULL,
-  `RSA_phoneNumber` INT(10) NOT NULL,
-  `timestamp` TIMESTAMP(6) NULL,
-  `callerLattitude` DECIMAL(2,2) NULL,
-  `callerLongtitude` DECIMAL(2,2) NULL,
-  PRIMARY KEY (`User_userID`, `RSA_phoneNumber`),
-  INDEX `fk_User_has_RSA_RSA1_idx` (`RSA_phoneNumber` ASC) VISIBLE,
-  INDEX `fk_User_has_RSA_User1_idx` (`User_userID` ASC) VISIBLE,
-  CONSTRAINT `fk_User_has_RSA_User1`
+    REFERENCES `evpointdb`.`chargingstation` (`companyName` , `latitude` , `longtitude`),
+  CONSTRAINT `fk_User_has_chargingStation_User1`
     FOREIGN KEY (`User_userID`)
-    REFERENCES `EVPointDB`.`User` (`userID`)
+    REFERENCES `evpointdb`.`user` (`userID`))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb3;
+
+
+-- -----------------------------------------------------
+-- Table `evpointdb`.`user_drives_car`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `evpointdb`.`user_drives_car` (
+  `car_licenseNumber` CHAR(7) NOT NULL,
+  `user_userID` CHAR(6) NOT NULL,
+  PRIMARY KEY (`car_licenseNumber`, `user_userID`),
+  INDEX `fk_car_has_user_user1_idx` (`user_userID` ASC) VISIBLE,
+  CONSTRAINT `fk_car_has_user_car1`
+    FOREIGN KEY (`car_licenseNumber`)
+    REFERENCES `evpointdb`.`car` (`licenseNumber`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
-  CONSTRAINT `fk_User_has_RSA_RSA1`
-    FOREIGN KEY (`RSA_phoneNumber`)
-    REFERENCES `EVPointDB`.`RSA` (`phoneNumber`)
+  CONSTRAINT `fk_car_has_user_user1`
+    FOREIGN KEY (`user_userID`)
+    REFERENCES `evpointdb`.`user` (`userID`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
-ENGINE = InnoDB;
-
-
--- -----------------------------------------------------
--- Table `EVPointDB`.`timestamps`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `EVPointDB`.`timestamps` (
-  `create_time` TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP,
-  `update_time` TIMESTAMP NULL);
-
-
--- -----------------------------------------------------
--- Table `EVPointDB`.`timestamps_1`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `EVPointDB`.`timestamps_1` (
-  `create_time` TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP,
-  `update_time` TIMESTAMP NULL);
-
-
--- -----------------------------------------------------
--- Table `EVPointDB`.`timestamps_2`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `EVPointDB`.`timestamps_2` (
-  `create_time` TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP,
-  `update_time` TIMESTAMP NULL);
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb3;
 
 
 SET SQL_MODE=@OLD_SQL_MODE;
